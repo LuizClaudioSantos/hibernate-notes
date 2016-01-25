d52a13aeb8276e87865f4ed031c78d9702c72543
LuizClaudioSantos <luizsan@gmail.com>
2016-01-25 17:30:21 -0500

    Refactoring tests

--- src/test/java/com/selfstudy/UserTest.java
+++ src/test/java/com/selfstudy/UserTest.java
@@ -3,8 +3,7 @@ package com.selfstudy;
 import com.selfstudy.entity.User;
 import com.selfstudy.util.HibernateUtil;
 import org.hibernate.Session;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.Date;
 
@@ -13,10 +12,11 @@ import java.util.Date;
  */
 public class UserTest {
 
-    private User user;
+    private static User user;
+    private static Session  session;
 
-    @Before
-    public void createUser(){
+    @BeforeClass
+    public static void initialize(){
         User user = new User();
         user.setFirstName("Zé");
         user.setLastName("das Couves");
@@ -26,29 +26,28 @@ public class UserTest {
         user.setLastUpdateBy("Alonso");
         user.setLastUpdateDate(new Date());
         user.setCreateBy("Alonso");
-        this.user = user;
+        UserTest.user = user;
+        UserTest.session = HibernateUtil.getSessionFactory().openSession();
+    }
+
+    @AfterClass
+    public static void finilize(){
+        UserTest.session.close();
     }
 
     @Test
-    public void XmlConfigurationTest(){
-
-        Session session = HibernateUtil.getSessionFactoryXML().openSession();
+    public void PropertiesConfigurationTest(){
         session.beginTransaction();
-
-        session.save(user);
-        session.close();
-
+        session.save(this.user);
+        session.getTransaction().commit();
     }
 
-    //@Test
-    public void PropertiesConfigurationTest(){
-
-        Session session = HibernateUtil.getSessionFactory().openSession();
+    @Test
+    public void updateUser(){
         session.beginTransaction();
-
-        session.save(user);
-        session.close();
-
+        User dbUser = (User) session.get(User.class, this.user.getUserId());
+        dbUser.setFirstName("Abreu");
+        session.getTransaction().commit();
     }
 
 }
